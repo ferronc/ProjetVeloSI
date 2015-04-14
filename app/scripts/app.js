f656576b120b63f34e8e257b40a395ac338056b5
FERRON Charles <charles@pc-san.home>
2015-04-14 12:15:41 +0200

    push de fin de scéance

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -77,18 +77,25 @@ angular
       .state('dashboard.home',{
         url:'/home',
         controller: 'MainCtrl',
-        templateUrl:'views/dashboard/home.html',
+        templateUrl:'views/dashboard/home.html',  
         resolve: {
-          loadMyFiles:function($ocLazyLoad) {
-            return $ocLazyLoad.load({
-              name:'sbAdminApp',
+          loadMyFile:function($ocLazyLoad) {
+            return $ocLazyLoad.load(
+                'bower_components/Chart.js/Chart.min.js'
+            ),
+            $ocLazyLoad.load({
+              name:'chart.js',
               files:[
-              'scripts/controllers/main.js',
-              'scripts/directives/timeline/timeline.js',
-              'scripts/directives/notifications/notifications.js',
-              'scripts/directives/chat/chat.js',
-              'scripts/directives/dashboard/stats/stats.js'
+                'bower_components/angular-chart.js/dist/angular-chart.min.js',
+                'bower_components/angular-chart.js/dist/angular-chart.css'
               ]
+            }),
+            $ocLazyLoad.load({
+                name:'sbAdminApp',
+                files:[
+                'scripts/controllers/main.js', 
+                'scripts/directives/dashboard/stats/stats.js'
+                ]
             })
           }
         }
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,6 +6,62 @@
  * # MainCtrl
  * Controller of the sbAdminApp
  */
+var d = new Date();
+var h = d.getHours();
+var j = d.getDay();
+var weekday = new Array(7);
+weekday[0]=  "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+function myFunction(aDate) {
+	var now = new Date ();
+	var tmp = now.getTime()-aDate.getTime();
+
+	if (tmp > 24*60*60*1000) {
+		return tmp+" j";
+	}
+	else if (tmp > 60*60*1000) {
+		return tmp+" h";
+	}
+	else if (tmp > 60*1000) {
+		return tmp+" min";
+	}
+	else if (tmp > 1000) {
+    	return tmp+" s";
+    }
+   	return tmp+" ms";
+}
+
+
 angular.module('sbAdminApp')
-  .controller('MainCtrl', function($scope,$position) {
-  });
+  .controller('MainCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+    $scope.line = {
+	    labels: [ h-6, h-5, h-4, h-3, h-2, h-1, h],
+	    series: ['Batterie'],
+	    data: [
+	      [65, 59, 80, 81, 56, 55, 40]
+	    ],
+	    onClick: function (points, evt) {
+	      console.log(points, evt);
+	    }
+    };
+
+    $scope.bar = {
+	    labels: [ weekday[1], weekday[2], weekday[3], weekday[4], weekday[5], weekday[6], weekday[0]],
+		series: ['Series A'],
+		data: [
+		   [65, 59, 80, 81, 56, 55, 40]
+		]
+    };
+
+    $scope.vitesseMax = { data : 6 };
+
+    $scope.dTotale = { data : 50 };
+    $scope.dMaxEntre2Charges = { data : 15 };
+    $scope.tpsSinceLastUse = { data : myFunction(new Date (d)) };
+}]);
